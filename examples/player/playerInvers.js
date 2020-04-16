de85e4b6d484dc433afefa1444a13161c83610a4
ATSISTEMAS\afernandez.iniesta <a.fernandezin@gmail.com>
2020-04-16 19:14:32 +0200

    player function

--- examples/player/playerInvers.js
+++ examples/player/playerInvers.js
@@ -1,87 +1,81 @@
-
-
-
-const listPlay=[
-  {
-    title: 'Rave Digger',
-    file: 'rave_digger',
-    howl: null
-  },
-  {
-    title: '80s Vibe',
-    file: '80s_vibe',
-    howl: null
-  },
-  {
-    title: 'Running Out',
-    file: 'running_out',
-    howl: null
-  }
-]
-
-
-function getTemplate(data){
-
-const html=`<li>
+/**
+ *  Template for every File
+ * @param {} data 
+ */
+function getTemplate(data) {
+  const html = `<li>
 <div class="card">
   <div class="card-content">
     <div class="content">
       <div class="name">${data.title}</div>
       <div class="description">${data.file}</div>
+      <div class="time"> </div>
       <div class="playaction action ">PLAY</div>
       <div class="stopaction action">STOP</div>
     </div>
   </div>
 </div>
 </li>`;
-return html;
-
+  return html;
 }
 
-
-function createElementItemPlayer(data){
-  const divItem = document.createElement('div');
-  divItem.innerHTML=getTemplate(data)
-  const play=divItem.querySelector('.playaction');
-  const stop=divItem.querySelector('.stopaction');
-  stop.style.display='none'
+/**
+ * Create elment DOM for sound File
+ * @param {*} data 
+ */
+function createElementItemPlayer(data) {
+  const divItem = document.createElement("div");
+  divItem.innerHTML = getTemplate(data);
+  const play = divItem.querySelector(".playaction");
+  const stop = divItem.querySelector(".stopaction");
+  const time = divItem.querySelector(".time");
+  stop.style.display = "none";
 
   data.howl = new Howl({
-    src: ['./audio/' + data.file + '.webm', './audio/' + data.file + '.mp3'],
+    src: ["./audio/" + data.file + ".webm", "./audio/" + data.file + ".mp3"],
     html5: true, // Force to HTML5 so that the audio can stream in (best for large files).
-    onplay: function() {
+    onplay: function () {
       // Display the duration.
-      
-    }
+    },
+    onload: function () {
+      const timeplay = data.howl.duration();
+      time.textContent = timeplay;
+    },
+
+    onend: function () {
+      play.style.display = "block";
+      stop.style.display = "none";
+    },
   });
-  
-  
-  play.addEventListener("click", function(){
+
+  play.addEventListener("click", function () {
     //alert('pulsado en'+data.title)
     data.howl.play();
-    play.style.display='none'
-    stop.style.display='block'
-  }); 
-
-  stop.addEventListener("click", function(){
-    //alert('pulsado en'+data.title)
-    data.howl.stop();
-    play.style.display='block'
-    stop.style.display='none'
+    play.style.display = "none";
+    stop.style.display = "block";
   });
 
+  stop.addEventListener("click", function () {
+    //alert('pulsado en'+data.title)
+    data.howl.stop();
+    play.style.display = "block";
+    stop.style.display = "none";
+  });
 
   return divItem;
 }
 
 
+/**
+ * Create Player list from listplay  
+ * @param {*} idContainer 
+ * @param {*} listPlay 
+ */
+function playerlist(idContainer,listPlay){
+  const playerCont = document.getElementById(idContainer);
 
-const playerCont = document.getElementById('player-container')
-
-
-listPlay.forEach(function(data){
-
-  playerCont.appendChild(createElementItemPlayer(data));    
-
-
-})
+  listPlay.forEach(function (data) {
+    playerCont.appendChild(createElementItemPlayer(data));
+  });
+  
+}   
